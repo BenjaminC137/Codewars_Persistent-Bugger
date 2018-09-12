b1fdbb56a74338cfaf665ee8c4272daad8af6a50
benjaminc137 <benjamin.salasek@icloud.com>
2018-09-11 20:34:02 -0500

    initial

---
+++ Codewars_Persistent-Bugger/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Codewars_Persistent_Bugger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Persistence(234));
+
+        }
+           public static int Persistence(long n) 
+        {
+        return 8;
+        }
+    }
+  
+}
+ 
