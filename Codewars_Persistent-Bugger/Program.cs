3fe36fe339165d8be03dfe9c2fd7997938d4d8aa
benjaminc137 <benjamin.salasek@icloud.com>
2018-09-11 22:01:03 -0500

    think I've got it.

--- Codewars_Persistent-Bugger/Program.cs
+++ Codewars_Persistent-Bugger/Program.cs
@@ -6,13 +6,26 @@ namespace Codewars_Persistent_Bugger
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Persistence(234));
+            Console.WriteLine(Persistence(39));
 
         }
            public static int Persistence(long n) 
         {
-        return 8;
-        }
+            int count;
+            for(count = 0; n > 9; count ++){
+                
+                  var stringN = n.ToString();
+                  n = 1;
+                  
+                  foreach(var number in stringN){
+                  string numph = number.ToString();
+                      int num = Int32.Parse(numph);
+                      n *= num;
+                  }   
+            }
+         return count;
+        }        
+        
     }
   
 }
